b260cfc8b9e5894a0c7ec88062c71c7a987a1b86
Aditya Ralhan <adityaralhan786@gmail.com>
2026-01-16 02:23:39 +0530

    Time: 3 ms (86.03%) | Memory: 48.3 MB (63.69%) - LeetSync

--- 495-teemo-attacking/teemo-attacking.java
+++ 495-teemo-attacking/teemo-attacking.java
@@ -2,7 +2,6 @@ class Solution {
     public int findPoisonedDuration(int[] timeSeries, int duration) {
         int val=duration-1;
         int ans=0;
-        ArrayList<Integer> list=new ArrayList<>();
         for(int i=0;i<timeSeries.length-1;i++){
             int time=timeSeries[i];
             int time2=timeSeries[i+1];
