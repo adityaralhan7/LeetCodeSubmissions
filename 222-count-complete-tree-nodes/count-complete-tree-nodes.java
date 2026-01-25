3c8b0c19e792c2800ca0b8dcc1c8fe841dcb1ebc
Aditya Ralhan <adityaralhan786@gmail.com>
2026-01-25 21:32:06 +0530

    Time: 0 ms (100.00%) | Memory: 49.4 MB (77.79%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.java
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.java
@@ -14,20 +14,12 @@
  * }
  */
 class Solution {
+    int ans=0;
     public int countNodes(TreeNode root) {
         if(root==null) return 0;
-        Queue<TreeNode> queue=new LinkedList<>();
-        int ans=0;
-        queue.offer(root);
-        while(!queue.isEmpty()){
-            int size=queue.size();
-            for(int i=0;i<size;i++){
-                ans++;
-                TreeNode n=queue.poll();
-                if(n.left!=null) queue.offer(n.left);
-                if(n.right!=null) queue.offer(n.right);
-            }
-        }
+        else ans++;
+        int left=countNodes(root.left);
+        int right=countNodes(root.right);
         return ans;
     }
 }
