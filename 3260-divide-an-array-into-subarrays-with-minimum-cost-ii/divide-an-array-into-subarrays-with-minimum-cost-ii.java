2293da1b651087c44ea869372d0a2fcfd76dfc2a
Aditya Ralhan <adityaralhan786@gmail.com>
2026-02-03 00:40:16 +0530

    Time: 365 ms (30.44%) | Memory: 103.6 MB (26.09%) - LeetSync

---
+++ 3260-divide-an-array-into-subarrays-with-minimum-cost-ii/divide-an-array-into-subarrays-with-minimum-cost-ii.java
@@ -0,0 +1,59 @@
+class Solution {
+    public long minimumCost(int[] nums, int k, int dist) {
+        TreeSet<Integer> currset = new TreeSet<>(
+            (a,b)->nums[a]!=nums[b]?nums[a]-nums[b]:a-b
+        );
+        TreeSet<Integer> futset = new TreeSet<>(
+            (a,b)->nums[a]!=nums[b]?nums[a]-nums[b]:a-b
+        );
+
+        long cost = Long.MAX_VALUE;
+        long currsum = 0L;
+        int n = nums.length;
+
+        for(int i=1;i<=dist+1;i++){
+            currset.add(i);
+            currsum+=nums[i];
+        }
+
+        while(currset.size()>k-1){
+            int ind=currset.pollLast();
+            currsum-=nums[ind];
+            futset.add(ind);
+        }
+
+        cost=currsum;
+
+        for(int i=1;i<n-1-dist;i++){
+            int rightInd=i+dist+1;
+            int leftInd=i;
+
+            if(currset.contains(leftInd)){
+                currsum-=nums[leftInd];
+                currset.remove(leftInd);
+                currsum+=nums[rightInd];
+                currset.add(rightInd);
+            }else{
+                futset.remove(leftInd);
+                futset.add(rightInd);
+            }
+
+            if(!futset.isEmpty()){
+                int minF=futset.first();
+                int maxC=currset.last();
+                if(nums[minF]<nums[maxC]){
+                    currsum-=nums[maxC];
+                    currset.remove(maxC);
+                    currset.add(minF);
+                    currsum+=nums[minF];
+                    futset.remove(minF);
+                    futset.add(maxC);
+                }
+            }
+
+            cost=Math.min(cost,currsum);
+        }
+
+        return cost+nums[0];
+    }
+}
