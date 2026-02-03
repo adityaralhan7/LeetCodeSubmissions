a3870aa2cfe10d8161a8ac25c1c5dd0d809f9342
Aditya Ralhan <adityaralhan786@gmail.com>
2026-02-03 22:24:12 +0530

    Time: 1 ms (59.87%) | Memory: 45.6 MB (64.97%) - LeetSync

--- 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.java
+++ 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.java
@@ -20,20 +20,14 @@ int result=0;
         return result;
     }
 
-    public void dfs(TreeNode root){
-        if(root==null) return;
-        int count=0;
-        int sum[]=findSum(root,count);
-        int avg=sum[0]/sum[1];
-        if(avg==root.val) result++;
-        dfs(root.left);
-        dfs(root.right);
-    }
-    public int[] findSum(TreeNode root,int count){
-        if(root==null) return new int[]{0,0};
-        count++;
-        int left[]=findSum(root.left,count);
-        int right[]=findSum(root.right,count);
-        return new int[]{left[0]+right[0]+root.val,left[1]+right[1]+1};
+    public int[] dfs(TreeNode root){
+       if(root==null) return new int[]{0,0};
+       int left[]=dfs(root.left);
+       int right[]=dfs(root.right);
+       int sum=left[0]+right[0]+root.val;
+       int count=left[1]+right[1]+1;
+       int avg=sum/count;
+       if(avg==root.val) result++;
+       return new int[]{sum,count};
     }
 }
