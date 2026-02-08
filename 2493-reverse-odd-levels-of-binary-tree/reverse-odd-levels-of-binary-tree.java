fc54808fde5fede91175079254cd1025c38aa652
Aditya Ralhan <adityaralhan786@gmail.com>
2026-02-08 11:54:58 +0530

    Time: 0 ms (100.00%) | Memory: 49.9 MB (51.98%) - LeetSync

--- 2493-reverse-odd-levels-of-binary-tree/reverse-odd-levels-of-binary-tree.java
+++ 2493-reverse-odd-levels-of-binary-tree/reverse-odd-levels-of-binary-tree.java
@@ -20,13 +20,12 @@ class Solution {
         return root;
     }
     public void reverse(TreeNode left,TreeNode right,int level){
-        if(left==null||right==null) return;
+        if(left==null) return;
         if(level%2!=0){
             int temp=left.val;
             left.val=right.val;
             right.val=temp;
         }
-        
         reverse(left.left,right.right,level+1);
         reverse(left.right,right.left,level+1);
     }
